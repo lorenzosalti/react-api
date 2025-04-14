ee608f7ba20bac146f36f2eda81dc913419de360
lorenzosalti <lorenzosalti@gmail.com>
2025-04-14 15:29:28 +0200

    feat: Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import axios from "axios"
 
 function App() {
 
+  const [actresses, setActresses] = useState([])
+
 
 
   function getActresses() {
     axios.get('https://www.freetestapi.com/api/v1/actresses')
       .then(response => {
         console.log(response.data);
+        setActresses(response.data)
       })
       .catch(error => console.log(error))
   }
@@ -21,6 +24,19 @@ function App() {
   return (
     <>
       <h1>Lista Hollywood</h1>
+
+      <hr />
+
+      <ul>
+        {actresses.map(actress => <li>
+          <h3>{actress.name}</h3>
+          <img src={actress.image} alt={actress.name} />
+          <p>Born in {actress.birth_year}</p>
+          <p>{actress.nationality}</p>
+          <p>{actress.biography}</p>
+          <p>{actress.awards}</p>
+        </li>)}
+      </ul>
     </>
   )
 }
