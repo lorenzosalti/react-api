8574a254ce5640cf0fe9b48266ea2fa48a49e607
lorenzosalti <lorenzosalti@gmail.com>
2025-04-14 15:22:32 +0200

    feat: Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
 
+import axios from "axios"
 
 function App() {
+
+
+
+  function getActresses() {
+    axios.get('https://www.freetestapi.com/api/v1/actresses')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => console.log(error))
+  }
+
+
+  useEffect(() => getActresses(), [])
 
 
   return (
